a4b778b05e083eac104d2e77535bba89db339c42
vankokos <vano9995@gmail.com>
2021-01-16 18:10:31 +0200

    responsive slider

--- js/scriptslider.js
+++ js/scriptslider.js
@@ -1,5 +1,17 @@
-$('.charts').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+
+    if ( $(window).width() <= 700) {
+        $('.charts').slick({
+            infinite:true,
+            slidesToShow:1,
+            slidesToScroll:1,
+        });
+    }
+    else {
+    $('.charts').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+    }
+
+      
